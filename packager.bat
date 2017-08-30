4b55913aaec0b1ed1c3dab9122b23f5db85a14ae
Vasyl Glodan <glodanif@users.noreply.github.com>
2017-08-30 17:54:04 +0300

    Update packager.bat

--- packager.bat
+++ packager.bat
@@ -2,9 +2,9 @@
 echo Start
 
 @rem list of ignored files and folders
-set "IGNORE=\.gitignore\.git\.idea\output\CONTRIBUTING.md\README.md\packager.bat\"
+set "IGNORE=\.gitignore\.git\.idea\.output\CONTRIBUTING.md\README.md\packager.bat\"
 
-if exist output @rd /s/q output
+if exist .output @rd /s/q .output
 
 @rem getting current directory name
 for %%* in (.) do set CURRENT_DIRECTORY=%%~nx*
@@ -24,30 +24,30 @@ for /f "tokens=*" %%a in (%CURRENT_DIRECTORY%.toc) do (
 echo Archive name: %ARCHIVE_NAME%
 
 @rem copying addon's files into a temporary directory
-if not exist output\%CURRENT_DIRECTORY% mkdir output\%CURRENT_DIRECTORY%
+if not exist .output\%CURRENT_DIRECTORY% mkdir .output\%CURRENT_DIRECTORY%
 
 setlocal enabledelayedexpansion
 for /d %%a in (*) do (
     if /i "!IGNORE:\%%~nxa\=!" equ "%IGNORE%" (
-        xcopy /v/z/e/i "%%a" "output\%CURRENT_DIRECTORY%\%%a"
+        xcopy /v/z/e/i "%%a" ".output\%CURRENT_DIRECTORY%\%%a"
     )
 )
 for %%a in (*.*) do (
     if /i "!IGNORE:\%%~nxa\=!" equ "%IGNORE%" (
-        xcopy /v/z "%%a" "output\%CURRENT_DIRECTORY%"
+        xcopy /v/z "%%a" ".output\%CURRENT_DIRECTORY%"
     )
 )
 endlocal
 
 @rem zipping using 7-Zip
-cd output
+cd .output
 set ZIP_PATH="C:\Program Files\7-Zip\7z.exe"
 if not exist %ZIP_PATH% (
     echo Cannot find 7-Zip application. Expected location %ZIP_PATH%
 ) else (
     echo Zipping...
     %ZIP_PATH% a -tzip "%ARCHIVE_NAME%" %CURRENT_DIRECTORY%
-    echo Successfully zipped: output/%ARCHIVE_NAME%
+    echo Successfully zipped: .output/%ARCHIVE_NAME%
 )
 
 @rd /s/q "%CURRENT_DIRECTORY%"
