81873b9cdeb0ada31b1709d9309a8f2fe8915080
Vasyl Glodan <glodanif@gmail.com>
2017-08-30 15:47:46 +0300

    Script itself

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # wow-addon-packager
-Batch script for compression an addon into a zip archive ignorring specific files
+Batch script for compression an addon into a zip archive ignoring specific files
+
+## Overview
+
+#### How to use
+1. Move packager.bat file into a root of your addon folder.
+2. Run.
+
+#### What you will get
+The script will create an _output_ folder in a root of your addon folder and compress addon's files into a zip archive with following name '_your_addon_directory version.zip_' (ex. 'CombatIndicator v1.2.zip').
+The version will be automatically fetched from a _.toc_ file.
+
+#### Ignore
+`IGNORE` variable contains names of all directories and files (not recursively) which you don't want to be included in a zip archive.
+Initially, this variable contains:
+   + .git
+   + .idea
+   + .gitignore
+   + output
+   + CONTRIBUTING.md
+   + README.md
+   + packager.bat
+   
+but you can edit it as you want.
+
+## Warning
+You are using an open source project!
+    
+I'm not responsible for lost files. Please do some research if you have any concerns 
+about features included in this script before using it! YOU are choosing to use this script.
---
+++ packager.bat
@@ -0,0 +1,53 @@
+@echo off
+echo Start
+
+@rem list of ignored files and folders
+set "IGNORE=\.gitignore\.git\.idea\output\CONTRIBUTING.md\README.md\packager.bat\"
+
+if exist output @rd /s/q output
+
+@rem getting current directory name
+for %%* in (.) do set CURRENT_DIRECTORY=%%~nx*
+
+@rem generating archive name based on version from a .toc file
+for /f "tokens=*" %%a in (%CURRENT_DIRECTORY%.toc) do (
+    set LINE=%%a
+    setlocal enabledelayedexpansion
+    if not "!LINE:Version=!"=="!LINE!" (
+        set "ARCHIVE_NAME=%CURRENT_DIRECTORY% !LINE:## Version: =!.zip"
+        goto :proceed
+    )
+    endlocal
+)
+
+:proceed
+echo Archive name: %ARCHIVE_NAME%
+
+@rem copying addon's files into a temporary directory
+if not exist output\%CURRENT_DIRECTORY% mkdir output\%CURRENT_DIRECTORY%
+
+setlocal enabledelayedexpansion
+for /d %%a in (*) do (
+    if /i "!IGNORE:\%%~nxa\=!" equ "%IGNORE%" (
+        xcopy /v/z/e/i "%%a" "output\%CURRENT_DIRECTORY%\%%a"
+    )
+)
+for %%a in (*.*) do (
+    if /i "!IGNORE:\%%~nxa\=!" equ "%IGNORE%" (
+        xcopy /v/z "%%a" "output\%CURRENT_DIRECTORY%"
+    )
+)
+endlocal
+
+@rem zipping using 7zip
+cd output
+set ZIP_PATH="C:\Program Files\7-Zip\7z.exe"
+if not exist %ZIP_PATH% (
+    echo Cannot find 7-Zip application. Expected location %ZIP_PATH%
+) else (
+    echo Zipping...
+    %ZIP_PATH% a -tzip "%ARCHIVE_NAME%" %CURRENT_DIRECTORY%
+    echo Successfully zipped: output/%ARCHIVE_NAME%
+)
+
+@rd /s/q "%CURRENT_DIRECTORY%"
